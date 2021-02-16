1170c9328c5c441e86317bd18f663a3e652c8296
AugustoCalado <augusto.calado11@gmail.com>
2021-02-16 16:16:48 -0300

    C4 Project - Added Lambda Function to get TODO Tasks

--- backend/src/lambda/http/getTodo.ts
+++ backend/src/lambda/http/getTodo.ts
@@ -1,24 +1,32 @@
 import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-import { getAllTodosTasks } from '../../businessLogic/todos'
+import { getTodoTask } from '../../businessLogic/todos'
 import { createLogger } from '../../utils/logger'
 import { getUserId } from '../utils'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 
-const logger = createLogger('getTodos')
+const logger = createLogger('getTodo')
 
-// DONE: Get all TODO items for a current user
 export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  logger.info('Processing get all TODOS event: ', event)
+  logger.info('Processing get TODO event: ', event)
+
   const userId = getUserId(event)
+  const todoId = event.pathParameters.todoId
 
-  const items = await getAllTodosTasks(userId)
+  const item = await getTodoTask(userId, todoId)
 
+  if (item.length === 0){
+    logger.info('Incorrect ID: ', todoId)
+    return {
+        statusCode: 404,
+        body: 'todoId does not exist'
+      }
+  }
   return {
     statusCode: 200,
     body: JSON.stringify({
-      items
+      item
     })
   }
 })
